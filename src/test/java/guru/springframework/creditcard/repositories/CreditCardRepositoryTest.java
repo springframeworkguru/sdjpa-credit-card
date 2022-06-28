fae422c16b2f69f588df9a38d2224fd7ef8ebe5e
John Thompson <john@springframework.guru>
2022-06-28 16:39:51 -0400

    adding persistence test

---
+++ src/test/java/guru/springframework/creditcard/repositories/CreditCardRepositoryTest.java
@@ -0,0 +1,39 @@
+package guru.springframework.creditcard.repositories;
+
+import guru.springframework.creditcard.domain.CreditCard;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("local")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CreditCardRepositoryTest {
+
+    final String CREDIT_CARD = "12345678900000";
+
+    @Autowired
+    CreditCardRepository creditCardRepository;
+
+    @Test
+    void testSaveAndStoreCreditCard() {
+        CreditCard creditCard = new CreditCard();
+        creditCard.setCreditCardNumber(CREDIT_CARD);
+        creditCard.setCvv("123");
+        creditCard.setExpirationDate("12/2028");
+
+        CreditCard savedCC = creditCardRepository.saveAndFlush(creditCard);
+
+        System.out.println("Getting CC from database");
+
+        CreditCard fetchedCC = creditCardRepository.findById(savedCC.getId()).get();
+
+        assertThat(savedCC.getCreditCardNumber()).isEqualTo(fetchedCC.getCreditCardNumber());
+
+    }
+}
