2f05c19b451798c6e45da5cf2cb5ff30426e0592
John Thompson <john@springframework.guru>
2022-06-30 13:20:47 -0400

    disabling event listeners

--- src/main/java/guru/springframework/creditcard/config/ListenerRegistration.java
+++ src/main/java/guru/springframework/creditcard/config/ListenerRegistration.java
@@ -3,18 +3,13 @@ package guru.springframework.creditcard.config;
 import guru.springframework.creditcard.listeners.PostLoadListener;
 import guru.springframework.creditcard.listeners.PreInsertListener;
 import guru.springframework.creditcard.listeners.PreUpdateListener;
-import org.hibernate.event.service.spi.EventListenerRegistry;
-import org.hibernate.event.spi.EventType;
-import org.hibernate.internal.SessionFactoryImpl;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by jt on 6/29/22.
  */
-@Component
+//@Component
 public class ListenerRegistration implements BeanPostProcessor {
 
     private final PostLoadListener postLoadListener;
@@ -35,16 +30,16 @@ public class ListenerRegistration implements BeanPostProcessor {
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
 
-        if (bean instanceof LocalContainerEntityManagerFactoryBean){
-            LocalContainerEntityManagerFactoryBean lemf = (LocalContainerEntityManagerFactoryBean) bean;
-            SessionFactoryImpl sessionFactory = (SessionFactoryImpl) lemf.getNativeEntityManagerFactory();
-            EventListenerRegistry registry = sessionFactory.getServiceRegistry()
-                    .getService(EventListenerRegistry.class);
-
-            registry.appendListeners(EventType.POST_LOAD, postLoadListener);
-            registry.appendListeners(EventType.PRE_INSERT, preInsertListener);
-            registry.appendListeners(EventType.PRE_UPDATE, preUpdateListener);
-        }
+//        if (bean instanceof LocalContainerEntityManagerFactoryBean){
+//            LocalContainerEntityManagerFactoryBean lemf = (LocalContainerEntityManagerFactoryBean) bean;
+//            SessionFactoryImpl sessionFactory = (SessionFactoryImpl) lemf.getNativeEntityManagerFactory();
+//            EventListenerRegistry registry = sessionFactory.getServiceRegistry()
+//                    .getService(EventListenerRegistry.class);
+//
+//            registry.appendListeners(EventType.POST_LOAD, postLoadListener);
+//            registry.appendListeners(EventType.PRE_INSERT, preInsertListener);
+//            registry.appendListeners(EventType.PRE_UPDATE, preUpdateListener);
+//        }
 
         return bean;
     }
