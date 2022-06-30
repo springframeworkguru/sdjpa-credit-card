7c35af389147a7de4d2421bbcaefa484bc6ac1c4
John Thompson <john@springframework.guru>
2022-06-30 14:02:01 -0400

    adding spring application context helper

---
+++ src/main/java/guru/springframework/creditcard/config/SpringContextHelper.java
@@ -0,0 +1,24 @@
+package guru.springframework.creditcard.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 6/30/22.
+ */
+@Component
+public class SpringContextHelper implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext(){
+        return applicationContext;
+    }
+}
